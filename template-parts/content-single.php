e723b0f8978aa5c505f809890b215398ce1b969c
Nadeem Abdulla <nadeemabd@gmail.com>
2016-03-05 20:56:21 +0530

    Content and content single style and category, tags markup

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -12,9 +12,20 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php
 		// Post thumbnail.
-		elegance_post_thumbnail();
+		if ( has_post_thumbnail() ) {
+			printf( '<div class="hero-image">' . the_post_thumbnail('elegance-hero-image') .	'</div>');
+		}
 	?>
+
 	<header class="entry-header">
+		<?php
+
+		$categories_list = get_the_category_list( esc_html__( ', ', 'elegance' ) );
+		if ( $categories_list && elegance_categorized_blog() ) {
+			printf( '<span class="cat-links">' . esc_html__( ' %1$s', 'elegance' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+		}
+
+		?>
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 		<div class="entry-meta">
@@ -34,6 +45,8 @@
 
 	<footer class="entry-footer">
 		<?php elegance_entry_footer(); ?>
+
+
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
 
