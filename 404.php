5737c342627d6e3cb8aa32ae83580aa84f33f55b
Nadeem Abdulla <nadeemabd@gmail.com>
2016-03-17 18:08:06 +0530

    Custom 404 template

--- 404.php
+++ 404.php
@@ -13,42 +13,18 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'elegance' ); ?></h1>
-				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'elegance' ); ?></p>
+					<div class="error-content">
+						<h1 class="error-header">404</h1>
+						<p><?php esc_html_e( 'Lost? Maybe try a search? Go ', 'elegance' ); ?>
+							<a href="<?php echo esc_url(home_url('/')); ?>" rel="home">home</a>
+							<?php esc_html_e( ' maybe?', 'elegance' ); ?>
+						</p>
 
-					<?php get_search_form(); ?>
+						<?php get_search_form(); ?>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( elegance_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'elegance' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'elegance' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
+					</div><!-- .error-content -->
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
