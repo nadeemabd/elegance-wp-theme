dfb1ebb17f85e476f6e1c8ed89f7ea6d6adf3bbf
Nadeem Abdulla <nadeemabd@gmail.com>
2016-03-18 18:21:33 +0530

    Change 'Primary Menu' to 'Menu'

--- header.php
+++ header.php
@@ -12,49 +12,52 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'elegance' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'elegance'); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php endif; ?>
-			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-		</div><!-- .site-branding -->
+    <header id="masthead" class="site-header" role="banner">
+        <div class="site-branding">
+            <?php if (is_front_page() && is_home()) : ?>
+                <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                          rel="home"><?php bloginfo('name'); ?></a></h1>
+            <?php else : ?>
+                <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                          rel="home"><?php bloginfo('name'); ?></a></h1>
+            <?php endif; ?>
+            <p class="site-description"><?php bloginfo('description'); ?></p>
+        </div><!-- .site-branding -->
 
-		<?php if (!is_404()): ?>
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-			<div class="site-header-wrapper">
-				<button id="menu-toggle" class="menu-toggle" aria-controls="primary-menu"
-						aria-expanded="false"><?php esc_html_e('Primary Menu', 'elegance'); ?></button>
-				<div id="site-header-menu" class="site-header-menu">
-					<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
-				</div>
-				<div id="search-container-wrapper" class="search-container-wrapper">
-					<div class="search-toggle">
-						<div class="search-icon"><i class="fa fa-search"></i></div>
-						<a href="#search-container" class="screen-reader-text"><?php _e('Search', 'elegance'); ?></a>
-					</div>
-					<div id="search-container" class="search-box-wrapper">
-						<div class="search-box">
-							<?php get_search_form(); ?>
-						</div>
-					</div>
-				</div>
-			</div>
-			</nav><!-- #site-navigation -->
-		<?php endif ?>
-	</header><!-- #masthead -->
+        <?php if (!is_404()): ?>
+            <nav id="site-navigation" class="main-navigation" role="navigation">
+                <div class="site-header-wrapper">
+                    <button id="menu-toggle" class="menu-toggle" aria-controls="primary-menu"
+                            aria-expanded="false"><?php esc_html_e('Menu', 'elegance'); ?></button>
+                    <div id="site-header-menu" class="site-header-menu">
+                        <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
+                    </div>
+                    <div id="search-container-wrapper" class="search-container-wrapper">
+                        <div class="search-toggle">
+                            <div class="search-icon"><i class="fa fa-search"></i></div>
+                            <a href="#search-container"
+                               class="screen-reader-text"><?php _e('Search', 'elegance'); ?></a>
+                        </div>
+                        <div id="search-container" class="search-box-wrapper">
+                            <div class="search-box">
+                                <?php get_search_form(); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav><!-- #site-navigation -->
+        <?php endif ?>
+    </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+    <div id="content" class="site-content">
